1e2b59e9cc3ae8def1c44ee94ea17e2017848764
Caleb Phillips <calebrphillips@gmail.com>
2016-12-26 10:01:08 -0500

    Abandom hope all ye who Clojure.

--- doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to bdd
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- src/bdd/buckets.clj
+++
@@ -1,9 +0,0 @@
-(ns bdd.buckets)
-
-(defn run [] 
-  (println "hello world"))
-
-(def num-buckets 10)
-(def num-items 10)
-(def bucket-capacity 1)
-
