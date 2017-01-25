bec8d9a5805731ea8dfa88d514b20d16f2ac68f8
Caleb Phillips <calebrphillips@gmail.com>
2017-01-25 14:57:10 -0500

    Add proposed front end for colourability problem

--- colours.py
+++ colours.py
@@ -1,3 +1,40 @@
+from solver import Solver, Constraint, Universal
 
+V = {1, 2, 3, 4, 5, 6}  # Vertices
 
-V = {0, 1, 2, 3, 4}
+E = {{1, 3}, {2, 3}, {3, 4}, {3, 5}, {2, 5}}  # Edges
+
+C = {'red', 'blue', 'green', 'yellow'}
+
+solver = Solver()
+
+solver.set(V)
+
+solver.map_to(C)
+
+v1 = Universal(V)
+v2 = Universal(V)
+# OR v1 = solver.universal()
+
+"""
+Possible also Single, Exists, None?
+"""
+
+c1 = Constraint()
+c1.NOT_IN({v1, v2}, E)
+
+c2 = Constraint()
+c2.NOT_EQU(v1, v2)
+
+c3 = Constraint()
+c3.AND(c1, c2)
+
+c4 = Constraint()
+c4.NOT_EQU(v1.mapped, v2.mapped)
+
+c_final = Constraint()
+c_final.IMP(c3, c4)
+
+solver.constrain(c_final)
+
+solver.execute()
