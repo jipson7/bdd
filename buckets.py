9948af3f35e9e342221ca5863d207d1c2b6e3edd
Caleb Phillips <calebrphillips@gmail.com>
2017-02-12 15:13:46 -0500

    Init commit of buckets example, declaration only

---
+++ buckets.py
@@ -0,0 +1,22 @@
+from solver import Generator, Block
+from functools import reduce
+
+X = 4  # Number of buckets
+Y = 4  # Number of unique items
+Z = 1  # Bucket Capacity
+
+block_matrix = []
+
+block_values = [True, False]
+
+for i in range(X):
+    block_matrix.append([])
+    for j in range(Y):
+        block_matrix[i].append(Block(block_values))
+
+block_list = reduce(lambda x, y: x+y, block_matrix)
+
+bdd = Generator(block_list)
+
+solutions = bdd.execute()
+print(solutions)
