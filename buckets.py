818df94c1662580d8c576f783903d7da5affe724
Caleb Phillips <calebrphillips@gmail.com>
2017-02-13 13:17:19 -0500

    Implement working buckets example

--- buckets.py
+++ buckets.py
@@ -14,15 +14,24 @@ for i in range(X):
     for j in range(Y):
         block_matrix[i].append(Block(block_values))
 
+# Flatten the block matrix to pass it to Generator
 block_list = reduce(lambda x, y: x+y, block_matrix)
-
 bdd = Generator(block_list)
 
-temp_block = bdd.apply('+', block_matrix[0])
+# Restrict Buckets to Capacity
+for i in range(X):
+    row = block_matrix[i]
+    temp_block = bdd.apply('+', row)
+    bdd.lte(temp_block, Z)
 
-bdd.lte(temp_block, Z)
+#Restrict item placement to 1
+for i in range(Y):
+    column = [row[i] for row in block_matrix]
+    temp_block = bdd.apply('+', column)
+    bdd.equ(temp_block, 1)
 
 solutions = bdd.execute()
-
 for s in solutions:
     print(s)
+
+print("There are " + str(len(solutions)) + " solution(s).")
