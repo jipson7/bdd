d420c3170c4bf9d87714211d57f273964d242ebd
Caleb Phillips <calebrphillips@gmail.com>
2017-03-20 12:37:29 -0400

    Separate colours example to sql and non sql versions

--- colours.py
+++ colours.py
@@ -1,41 +1,17 @@
+from itertools import combinations
 from solver import Generator, Block
-from sqlalchemy import create_engine, Column, Integer
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 
-"""
-Sqlalchemy Setup
-"""
-engine = create_engine('sqlite:///databases/colours.db')
-db = (sessionmaker(bind=engine))()
-Base = declarative_base()
-
-
-class Edge(Base):
-    __tablename__ = 'edge'
-    v1 = Column(Integer, primary_key=True)
-    v2 = Column(Integer, primary_key=True)
-
-
-class Vertex(Base):
-    __tablename__ = 'vertex'
-    v = Column(Integer, primary_key=True)
-
-
-"""
-Run BDD test
-"""
 colours = ['red', 'yellow', 'green', 'blue']
-num_v = db.query(Vertex).count()
+num_v = 4
+edges = [list(x) for x in combinations(range(num_v), 2)]
 
 blocks = [Block(colours) for _ in range(num_v)]
 
 bdd = Generator(blocks)
 
-for e in db.query(Edge):
-    bdd.not_equ(blocks[e.v1], blocks[e.v2])
+for e in edges:
+    bdd.not_equ(blocks[e[0]], blocks[e[1]])
 
 solutions = bdd.execute()
 
-for s in solutions:
-    print(s)
+print(len(solutions))
